ba602cc654013f5e4bfdd86faf33e4c5b2ba6d48
yosuke-ishikawa <yosuke1028.ishikawa@gmail.com>
2021-03-09 15:35:52 +0900

    設定ファイルの変更

---
+++ TodoApp/TodoApp/CheckBox.swift
@@ -0,0 +1,41 @@
+//
+//  CheckBox.swift
+//  TodoApp
+//
+//  Created by Yosuke Ishikawa on 2021/02/16.
+//
+
+import SwiftUI
+
+struct CheckBox<Label>: View where Label: View {
+    @Binding var checked: Bool
+    private var label: () -> Label
+    
+    public init(checked: Binding<Bool>, @ViewBuilder label: @escaping () -> Label) {
+        self._checked = checked
+        self.label = label
+    }
+    
+    var body: some View {
+        HStack {
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
+            }
+            label()
+        }
+    }
+}
+
+struct CheckBox_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CheckBox(checked: .constant(false)) {
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(true)) {
+                Text("勉強をする")
+            }
+        }
+    }
+}
